1705b110d4d8179acb7e1c0c6646c5810da8ef50
Anshika Gupta <anshikagupta1704@gmail.com>
2026-06-06 07:34:31 +0530

    Fixed error code

--- core/controllers/auth_controllers.go
+++ core/controllers/auth_controllers.go
@@ -12,14 +12,20 @@ var req models.RegisterUser
 func Register(w http.ResponseWriter, r *http.Request) {
 	json.NewDecoder(r.Body).Decode(&req)
 	hash_pass, err := password.HashPass(req.Password)
+
 	if err != nil {
-		return "", err
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
 	}
+
 	if req.Username == "" {
-
+		http.Error(w, "Username is required", http.StatusBadRequest)
+		return
 	}
-	if req.Password == "" {
 
+	if req.Password == "" {
+		http.Error(w, "Password is required", http.StatusBadRequest)
+		return
 	}
 
 	user := core.User{
