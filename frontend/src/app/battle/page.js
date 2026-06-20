11b17595d4a12cb4428ab85a4f4e832d3aa14dd2
Anshika Gupta <anshikagupta1704@gmail.com>
2026-06-20 18:12:53 +0530

    Changed battle screen

--- frontend/src/app/battle/page.js
+++ frontend/src/app/battle/page.js
@@ -251,7 +251,7 @@ export default function BattlePage() {
 
             {battlePhase === "fighting" && (
                 <div className="fixed inset-0 bg-black flex flex-col items-center justify-center z-50 text-white">
-                    <p className="text-3xl animate-pulse">{battleMessage}</p>
+                    <p className="text-3xl">{battleMessage}</p>
                 </div>
             )}
 
